b34f3942db107980b8b4cdcf0191f83aa7c75701
BatraAayush <67547222+BatraAayush@users.noreply.github.com>
2022-10-01 15:54:16 +0530

    minor change

--- app.js
+++ app.js
@@ -122,6 +122,10 @@ app.post("/delete", async function (req, res) {
   }
 });
 
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
